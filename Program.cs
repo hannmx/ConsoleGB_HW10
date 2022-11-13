e97361751cda681a1b888f55d884964cd81a2eff
hannmx <mxkud@mail.ru>
2022-11-13 16:42:40 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGB_HW10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int num = rand.Next(100, 1000);
+            int result = 0;
+            void Number()
+            {
+                Console.WriteLine("Случайное трёхзначное число: " + num);
+                result = (num - (num / 100) * 100 - num % 10) / 10;
+                Console.WriteLine($"Вторая цифра числа {num}: {result}");
+            }
+            Number();
+        }
+    }
+}
